3ee838e10444f65775ecea85376633714dcc8f13
VladWick <91905024+VladWick@users.noreply.github.com>
2022-06-15 23:22:40 +0700

    Fix bug with pom.xml

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -1,16 +1,20 @@
 package com.example.demo.appuser;
 
-import com.example.demo.registration.token.ConfirmationToken;
-import com.example.demo.registration.token.ConfirmationTokenService;
-import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+import com.example.demo.registration.token.ConfirmationToken;
+import com.example.demo.registration.token.ConfirmationTokenRepository;
+import com.example.demo.registration.token.ConfirmationTokenService;
+
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
@@ -19,7 +23,12 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG =
             "user with email %s not found";
 
+    @Autowired
     private final AppUserRepository appUserRepository;
+    
+    @Autowired
+	private final ConfirmationTokenRepository confirmationTokenRepository;
+	
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ConfirmationTokenService confirmationTokenService;
 
@@ -37,33 +46,29 @@ public class AppUserService implements UserDetailsService {
                 .findByEmail(appUser.getEmail())
                 .isPresent();
 
-        if (userExists) {
-            // TODO check of attributes are the same and
-            // TODO if email not confirmed send confirmation email.
-
-            throw new IllegalStateException("email already taken");
-        }
+        if(userExists) {
+			if(appUser.isEnabled()) {
+				throw new IllegalStateException("email already taken");
+			}
+			AppUser existingUser = appUserRepository.findByEmail(appUser.getEmail()).get();
+			confirmationTokenRepository.delete(confirmationTokenRepository.findById(existingUser.getId()+1).get());
+			appUserRepository.delete(existingUser);
+		}
 
         String encodedPassword = bCryptPasswordEncoder
                 .encode(appUser.getPassword());
-
         appUser.setPassword(encodedPassword);
-
         appUserRepository.save(appUser);
 
         String token = UUID.randomUUID().toString();
-
         ConfirmationToken confirmationToken = new ConfirmationToken(
                 token,
                 LocalDateTime.now(),
                 LocalDateTime.now().plusMinutes(15),
                 appUser
         );
-
         confirmationTokenService.saveConfirmationToken(
                 confirmationToken);
-
-//        TODO: SEND EMAIL
 
         return token;
     }
